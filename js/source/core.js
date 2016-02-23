5cc0a016214f21e6e2a98ae09f251f5b839182ce
Mario Vallejo <mario@vallgroup.com>
2016-02-23 17:11:40 -0600

    jquery UI date picker

--- js/source/core.js
+++ js/source/core.js
@@ -3,6 +3,14 @@
 
 	$(document).ready(function(){
 		PremiseTimeTrack.init();
+
+		// jQuery UI datepicker
+		if ( typeof $.datepicker === 'object' ) {
+			$( ".datepicker" ).datepicker({
+				inline: true,
+				minDate: 0, // minimum date: today
+			});
+		}
 	});
 
 	var PremiseTimeTrack = {
@@ -67,6 +75,8 @@
 			this.resetTimer();
 
 			this.bindEvents();
+
+			console.log('Premise Time Track initiated successfully');
 		},
 
 
