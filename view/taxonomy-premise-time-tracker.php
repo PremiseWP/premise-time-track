3210ea7b2e78630ae82a6b4b51f051e99b447b40
Mario Vallejo <mario@vallgroup.com>
2016-12-02 10:18:38 -0600

    removed permission check for now

--- view/taxonomy-premise-time-tracker.php
+++ view/taxonomy-premise-time-tracker.php
@@ -12,8 +12,29 @@ get_header();
 ob_start();
 
 if ( have_posts() ) :
+
 	// check if the user can view this
-	if ( ! current_user_can( 'administrator' ) && ! current_user_can( 'pwptt_client' ) ) {
+	// ! current_user_can( 'administrator' )
+
+	/**
+	 * Holds array with all the clients this user has access to. using the client slug as the key and 1 or 0 to determine access.
+	 *
+	 * i.e. array( "two-by-four" => "1" )
+	 *
+	 * @var array
+	 */
+	// $client_access = (array) premise_get_value( 'ptt_user_profile[client-access]', array( 'context' => 'user', 'id' => get_current_user_id() ) );
+
+	// foreach ( $client_access as $client => $bool ) {
+	// 	if ( is_tax( 'premise_time_tracker_client', $client ) ) {
+	// 		var_dump('yes! - ' . $client ) . '<br>';
+	// 	}
+	// 	else {
+	// 		var_dump('no! - ' . $client ) . '<br>';
+	// 	}
+	// }
+
+	if ( ! current_user_can( 'administrator' ) ) {
 		?><p class="pwptt-error-message">You do not have enough permissions to see this.</p><?
 	}
 	// User can view this
@@ -49,11 +70,12 @@ $pwptt_loop = ob_get_clean(); ?>
 
 			<div class="pwptt-header premise-clear-float">
 					<div class="pwptt-search-wrapper">
-						<?php premise_field( 'text', array(
-							'id'          => 'pwptt-search-timesheet',
-							'placeholder' => 'search, filter or sort',
-							'class'       => 'pwptt-search',
-						) ); ?>
+						<?php
+						// premise_field( 'text', array(
+						// 	'id'          => 'pwptt-search-timesheet',
+						// 	'placeholder' => '10/24/12 - 12/24/13',
+						// 	'class'       => 'pwptt-search',
+						// ) ); ?>
 					</div>
 					<div class="pwptt-total-wrapper">
 						<p class="pwptt-total">
